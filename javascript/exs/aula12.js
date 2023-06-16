67a869d388789886848443ed28c2d9a8a44155f8
joaovictoraraujodev <123034411+joaovictoraraujodev@users.noreply.github.com>
2023-06-16 17:58:56 -0300

    Add files via upload

---
+++ javascript/exs/aula12.js
@@ -0,0 +1,12 @@
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16 ){
+    console.log('não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')
+}
+//} else if (idade >= 18) {
+else {
+    console.log('Voto Obrigátorio')
+}
+                        
---
+++ javascript/exs/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75
+console.log(`a velocidade do seu carro é ${vel} por hora`)
+if (vel > 70) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`) 
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
