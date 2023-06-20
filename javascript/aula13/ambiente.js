5c2295783155b37e603895e068ce9c2871938c8b
joaovictoraraujodev <123034411+joaovictoraraujodev@users.noreply.github.com>
2023-06-20 17:41:00 -0300

    Add files via upload

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,24 @@
+// TESTE LÓGICO NO FINAL
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+//TESTE LÓGICO NO INICIO
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+/* SEM REPETIÇÃO
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)*/
