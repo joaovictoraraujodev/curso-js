26ef525a5278f923032e17acbe7b1ffad89576d9
joaovictoraraujodev <123034411+joaovictoraraujodev@users.noreply.github.com>
2023-06-22 17:36:38 -0300

    Delete javascript directory

--- javascript/exs/ex013.js
+++
@@ -1,9 +0,0 @@
-var agora = new Date()
-var dia = agora.getDate()
-console.log(`Hoje é dia ${dia}.`)
-if (dia <=15){
-    console.log('Estamos no inicio do mês')
-}
-else{
-    console.log('Estamos no final do mês')
-}
